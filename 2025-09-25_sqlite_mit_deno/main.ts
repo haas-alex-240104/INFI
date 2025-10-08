82ba58c8a195f09b6d3c77451d638e9b59cd709f
Alexander Haas <Haa240104@spengergasse.at>
2025-10-08 14:23:36 +0200

    Screenshot

--- 2025-09-25_sqlite_mit_deno/main.ts
+++ 2025-09-25_sqlite_mit_deno/main.ts
@@ -4,3 +4,6 @@ const db = new DatabaseSync("2ahwii.db");
 const stmt = db.prepare("SELECT * FROM students");
 const rows = stmt.all();
 console.log(rows);
+
+const insertStmt = db.prepare("INSERT INTO students (id, name, birthdate) VALUES (?, ?, ?)");
+insertStmt.run(249, 'Nora', '2010-05-15');
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # INFI
+2025.10.02
+Leider kann ich trotz der Hilfe von Copilot und allen tipps Nichts ind die Datenbank einfügen. Ich weiß auch nicht woran das liegt :(
+     
