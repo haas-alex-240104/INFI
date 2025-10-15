7330d8fef56e0ab0c4ffc381b3d606d948adffb7
alex haas <HAA240104@spengergasse.at>
2025-10-15 13:27:27 +0200

    Create Hü_2025-10-09.sql

---
+++ Hü_2025-10-09.sql
@@ -0,0 +1,5 @@
+sqlite> select count() from customers where City = 'Berlin';
+sqlite> select CustomerName, City, Country from customers;
+sqlite> select CustomerID, CustomerName from customers where City = 'London';
+sqlite> select * from products where Price >20;
+sqlite> select * from products where Price != 18;
